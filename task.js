8b34849102bcc57f81efa3a65d24bedc79ace527
irinafilipenko <73819257+irinafilipenko@users.noreply.github.com>
2021-04-05 23:56:18 +0300

    первые попытки

--- task.js
+++ task.js
@@ -12,7 +12,11 @@ function createImgGallery(gallerys) {
             return `
              
         <li>
-            <img src = '${gallery.preview}' alt = '${gallery.description}'>
+            <img class = "gallery-image"
+            src = '${gallery.preview}'
+            alt = '${gallery.description}'
+            width = 392
+            height = 240>
         </li>
     
     `;
@@ -20,4 +24,57 @@ function createImgGallery(gallerys) {
         .join(' ');
     
 };
-console.log(createImgGallery(gallerys));
+// console.log(createImgGallery(gallerys));
+
+const ImageEl = document.querySelector('.gallery-image');
+console.log(ImageEl.src);
+
+imgContainer.addEventListener('click', onContainerClick);
+
+
+
+const modalContainer = document.querySelector('.lightbox');
+
+console.log(modalContainer);
+
+function onContainerClick(event) {
+    const isGallaryEl = event.target.classList.contains('gallery-image');
+    if (!isGallaryEl) {
+        return;
+    }
+
+    const currentActiveImg = document.querySelector('.lightbox .is-open')
+    if (currentActiveImg) {
+        currentActiveImg.classList.remove('is-open');
+    }
+   
+   modalContainer.classList.add('is-open');
+    console.log(modalContainer);
+    
+};
+
+const closeButtonEl = document.querySelector('.lightbox__button');
+console.log(closeButtonEl);
+
+
+
+closeButtonEl.addEventListener('click', onCloseButton);
+
+function onCloseButton(event) {
+    
+    modalContainer.classList.remove('is-open');
+}
+
+// const modaleContainer = document.querySelector('.lightbox__content');
+// modaleContainer.addEventListener('click', makeOriginalImgEl);
+
+// function makeOriginalImgEl(gallerys) {
+//     return gallerys
+//         .map(gallery => {
+//             return `
+//               <img class="lightbox__image" src="${gallery.original}" alt="${gallery.description}" />
+//               `
+//         })
+//          .join(' ');
+// };
+// console.log(makeOriginalImgEl(gallerys));
